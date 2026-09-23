d02a162d419df8e53e4c5361109c73b88f03851c
Alexey Lashuk <aleksey.lashuk2021@gmail.com>
2026-09-23 16:38:25 +0300

    task done

--- src/undirected/connected.clj
+++ src/undirected/connected.clj
@@ -9,26 +9,27 @@
          remaining #{start}]
     (if (empty? remaining)
       (make-graph visited
-                  (???))
+                  (filter #(set/subset? (e/ends %) visited)
+                          (edges graph)))
       (let [adjacent (set (mapcat (fn [v] (adjacent-vertices graph v))
                                   remaining))
             unvisited (set/difference adjacent visited)]
-        (recur (???)
-               (???))))))
+        (recur (set/union visited remaining)
+               unvisited)))))
 
 (defn connected-components [graph]
   {:post [(mutually-disjoint? %)]}
   (loop [components []
          remaining (vertices graph)]
     (if (empty? remaining)
-      (???)
+      components
       (let [v (first remaining)
             comp (connected-component graph v)]
-        (recur (???)
-               (???))))))
+        (recur (conj components comp)
+               (set/difference remaining (vertices comp)))))))
 
 (defn connected-vertices? [graph, v1, v2]
-  (???))
+  (contains? (vertices (connected-component graph v1)) v2))
 
 (defn connected? [graph]
-  (???))
+  (<= (count (connected-components graph)) 1))
--- src/undirected/route.clj
+++ src/undirected/route.clj
@@ -17,34 +17,35 @@
   (:edges route))
 
 (defn length [route]
-  (???))
+  (count (edges route)))
 
 (defn start [route]
-  (???))
+  (first (vertices route)))
 
 (defn end [route]
-  (???))
+  (last (vertices route)))
 
 (defn contains-vertex? [route vertex]
-  (???))
+  (boolean (some #(= vertex %) (vertices route))))
 
 (defn add-vertex [route vertex]
-  (???))
+  (make-route (conj (vertices route) vertex)))
 
 (defn subroute? [subroute route]
-  (???))
+  (subsequence? (vertices sub) (vertices route)))
 
 (defn route->graph [route]
-  (???))
+  (g/make-graph (set (vertices route))
+                (set (edges route))))
 
 (defn graph-contains-route? [graph route]
-  (???))
+  (g/subgraph? (route->graph route) graph))
 
 (defn routes-same-graph? [r1 r2]
-  (???))
+  (= (route->graph r1) (route->graph r2)))
 
 (defn chain? [route]
-  (???))
+  (all-distinct? (edges route)))
 
 (defn- simple? [route]
   (or (all-distinct? (vertices route))
@@ -52,38 +53,43 @@
            (all-distinct? (rest (vertices route))))))
 
 (defn simple-chain? [route]
-  (???))
+  (and (simple? route)
+       (chain? route)))
 
 (defn cyclic? [route]
-  (???))
+  (= (start route) (end route)))
 
 (defn cycle? [route]
-  (???))
+  (and (cyclic? route)
+       (chain? route)))
 
 (defn simple-cycle? [route]
-  (???))
+  (and (cyclic? route)
+       (simple-chain? route)))
 
 (defn- extract-simple-chain-from [vertices]
   (loop [chain []
          remaining vertices]
     (if (empty? remaining)
       chain
-      (let [v (???)
-            chain-without-v (???)]
-        (recur (???)
-               (???))))))
+      (let [v (first remaining)
+            chain-without-v (if (some #{v} chain)
+                              (vec (take (inc (.indexOf chain v)) chain))
+                              chain)]
+        (recur (conj chain-without-v v)
+               (rest remaining))))))
 
 (defn extract-simple-chain [route]
   {:pre  [(not (cyclic? route))]
    :post [(subroute? % route) (simple-chain? %)]}
-  (make-route (extract-simple-chain-from (???))))
+  (make-route (extract-simple-chain-from (vertices route))))
 
 (defn extract-simple-cycle [route]
   {:pre  [(cyclic? route)]
    :post [(subroute? % route) (simple-cycle? %)]}
   (let [vs (vertices route)]
-    (make-route (cons (???)
-                      (extract-simple-chain-from (???))))))
+    (make-route (cons (start route)
+                      (extract-simple-chain-from (rest vs))))))
 
 (defn find-simple-cycle [graph]
   {:pre  [(every? (fn [d] (>= d 2)) (g/degrees graph))]
@@ -94,7 +100,7 @@
            last v0
            current v1]
       (if (some #{current} passed)
-        (make-route (???))
-        (recur (???)
-               (???)
-               (???))))))
+        (make-route (conj (subvec passed (.indexOf passed current)) current))
+        (recur (conj passed current)
+               current
+               (first (disj (g/adjacent-vertices graph current) last)))))))
